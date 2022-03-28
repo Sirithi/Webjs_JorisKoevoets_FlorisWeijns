38f170c43c4bd1e883fbcf412f3e2bc62741dd38
Joris Koevoets <63403671+Joris914@users.noreply.github.com>
2022-03-28 10:27:07 +0200

    add draggable on conveyor canvas

--- hallController.js
+++ hallController.js
@@ -2,6 +2,7 @@ import{ Conveyor } from './Conveyor.js'
 import { drawParcel } from './conveyorController.js';
 export function addNewConveyor(hall){
     let canvas = document.createElement('canvas');
+    canvas.draggable = "true";
     document.getElementById('conveyors').append(canvas);
 
     let ctx = canvas.getContext('2d')
